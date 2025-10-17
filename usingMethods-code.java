f7eca1d72854a747dfcae3068d389c0a22d80ac7
Lujille06 <crisartuz.c@gmail.com>
2025-10-17 12:46:10 +0800

    Update usingMethods-code.java

--- usingMethods-code.java
+++ usingMethods-code.java
@@ -20,48 +20,51 @@ class MethodOOP {
         String again;
   
         do{
-        System.out.println("-------------------------");
-        System.out.println("    SIMPLE CALCULATOR    ");
-        System.out.println("-------------------------");
-        System.out.print("Enter first number: ");
-        a = scanner.nextDouble();
-        System.out.print("Enter second number: ");
-        b = scanner.nextDouble();
-        
-        System.out.println("\n==== SELECT OPERATION ===");
-        System.out.println("1. ADD");
-        System.out.println("2. SUBTRACT");
-        System.out.println("3. MULTIPLY");
-        System.out.println("4. DIVIDE");
-        System.out.print("Enter your choice (1-4): ");
-        choice = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println();
-        switch (choice){
-            case 1:
-                result = sum(a,b);
-                System.out.println("Result: " + result);
-                break;
-            case 2:
-                result = difference(a,b);
-                System.out.println("Result: " + result);
-                break;
-            case 3:
-                result = product(a,b);
-                System.out.println("Result: " + result);
-                break;
-            case 4:
-                result = quotient(a,b);
-                System.out.println("Result: " + result);
-                break;
-            default: 
-                System.out.println("[ERROR] Invalid operation");
-                break;
-        }
-        System.out.print("\nDo you want to continue? (yes/no): ");
-        again = scanner.nextLine();
-        System.out.println();
+            System.out.println("-------------------------");
+            System.out.println("    SIMPLE CALCULATOR    ");
+            System.out.println("-------------------------");
+            System.out.print("Enter first number: ");
+            a = scanner.nextDouble();
+            System.out.print("Enter second number: ");
+            b = scanner.nextDouble();
+            
+            System.out.println("\n==== SELECT OPERATION ===");
+            System.out.println("1. ADD");
+            System.out.println("2. SUBTRACT");
+            System.out.println("3. MULTIPLY");
+            System.out.println("4. DIVIDE");
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println();
+            
+            switch (choice){
+                case 1:
+                    result = sum(a,b);
+                    System.out.println("Result: " + result);
+                    break;
+                case 2:
+                    result = difference(a,b);
+                    System.out.println("Result: " + result);
+                    break;
+                case 3:
+                    result = product(a,b);
+                    System.out.println("Result: " + result);
+                    break;
+                case 4:
+                    result = quotient(a,b);
+                    System.out.println("Result: " + result);
+                    break;
+                default: 
+                    System.out.println("[ERROR] Invalid operation");
+                    break;
+            }
+            
+            System.out.print("\nDo you want to continue? (yes/no): ");
+            again = scanner.nextLine();
+            System.out.println();
         } while (again.equals("yes") || again.equals("YES"));
+        
         System.out.println("--- Exit Program ---");
         scanner.close();
         } 
